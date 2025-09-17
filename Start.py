2f04a66a0c25f214823ef2f42de655d0bd79e139
Curso Basico Python <dailmaab@gmail.com>
2025-09-17 14:01:51 -0300

    Atualizando arquivos do curso basico

--- Start.py
+++ Start.py
@@ -9,15 +9,14 @@ var_DiasSemana.extend(var_meses)
 var_meses.remove('Março')
 var_Lista_Funcionario = open('Lista_Funcionarios.txt', 'r')
 
-print('A funcionária ' + var_Nome + ' da empresa ' + var_Empresa + ' trabalha ' + str(var_DiasSuporte) + ' de Suporte na semana.')
+print('A funcionária ' + var_Nome + ' da empresa ' + var_Empresa + ' trabalha ' + str(var_DiasSuporte) + ' dias de Suporte na semana.')
 print('A funcionária '+var_Nome + ' trabalhará ' + str(int(var_DiasSuporte) * int(var_Semana)) + ' dias de suporte no Mes.')
 print('Dias Suporte Mes: ' + str(var_DiasSuporteMes))
 print(var_DiasSemana[1:3])
-print(var_DiasSemana)
+print('Dias da Semana com extensão dos meses: '+str(var_DiasSemana))
 print(var_meses)
-print(var_meses.index('Janeiro'))
-print(var_Lista_Funcionario)
-
+print('O mês de Janeiro está na posição: '+ var_meses.index('Janeiro'))
+print('Lista de Funcionários da empresa '+var_Empresa + ' : '+var_Lista_Funcionario.read())
 
 
 
